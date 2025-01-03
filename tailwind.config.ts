1c4f08525976e69f32640d0a207ae54cb9900f8a
Sanmandresy <98638893+Sanmandresy@users.noreply.github.com>
2025-01-03 14:01:21 +0300

    chore: add noto font to project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./pages/**/**.html", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        noto: ["Noto Sans JP", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
